c2391297e2cf13bd86ea931d4bde5296a480c41b
SUGATACHANDA <scbabai2704@gmail.com>
2025-06-28 14:09:00 +0530

    Add route for fetching latest booking details

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
-const { getMyBookings } = require('../controllers/bookingController');
+const { getMyBookings, getLatestBooking } = require('../controllers/bookingController');
 
 router.route('/mybookings').get(protect, getMyBookings);
+router.route('/latest').get(protect, getLatestBooking);
 
 module.exports = router;
